0b8a649d748f52052bef78835e271f50ca9f8c82
kaneko <kaneko@daniel-soft.com>
2022-09-15 15:32:50 +0900

    検索条件に一致した商品がなかった場合のメッセージ表示機能の追加

--- ShoppingCart/ListShower.vb
+++ ShoppingCart/ListShower.vb
@@ -1,14 +1,19 @@
-﻿Public Class ListShower
+﻿Imports Fhi.Fw
+Public Class ListShower
     ''' <summary>
     ''' 渡されたリストを表示する
     ''' </summary>
     ''' <param name="goodsList">表示するリスト</param>
     Public Sub ShowList(goodsList As List(Of GoodsListMaker.GoodsProperty))
-        For Each list In goodsList
-            Console.WriteLine("商品名：" & list.goodsName & vbCrLf & "商品説明：" & list.goodsDescription & vbCrLf &
-                          "価格：" & list.goodsPrice & "円" & vbCrLf & "在庫数：" & list.goodsInventory)
-            Console.WriteLine()
-        Next
+        If StringUtil.IsNotEmpty(goodsList) Then
+            For Each list In goodsList
+                Console.WriteLine("商品名：" & list.goodsName & vbCrLf & "商品説明：" & list.goodsDescription & vbCrLf &
+                              "価格：" & list.goodsPrice & "円" & vbCrLf & "在庫数：" & list.goodsInventory)
+                Console.WriteLine()
+            Next
+        Else
+            Console.WriteLine("検索条件に一致する商品が見つかりませんでした")
+        End If
     End Sub
 
 End Class
