3b358da2c951a450bc58113681de74c69e10edf5
kaneko <kaneko@daniel-soft.com>
2022-09-15 14:57:38 +0900

    商品リストを表示する機能の追加

---
+++ ShoppingCart/ListShower.vb
@@ -0,0 +1,14 @@
+﻿Public Class ListShower
+    ''' <summary>
+    ''' 渡されたリストを表示する
+    ''' </summary>
+    ''' <param name="goodsList">表示するリスト</param>
+    Public Sub ShowList(goodsList As List(Of GoodsListMaker.GoodsProperty))
+        For Each list In goodsList
+            Console.WriteLine("商品名：" & list.goodsName & vbCrLf & "商品説明：" & list.goodsDescription & vbCrLf &
+                          "価格：" & list.goodsPrice & "円" & vbCrLf & "在庫数：" & list.goodsInventory)
+            Console.WriteLine()
+        Next
+    End Sub
+
+End Class
--- ShoppingCart/Main.vb
+++ ShoppingCart/Main.vb
@@ -1,7 +1,10 @@
 ﻿Module Main
 
     Sub Main()
-
+        Dim plm As New GoodsListMaker
+        Dim goodsList As List(Of GoodsListMaker.GoodsProperty) = plm.RegisterGoodsList
+        Dim ls As New ListShower
+        ls.ShowList(goodsList)
     End Sub
 
 End Module
