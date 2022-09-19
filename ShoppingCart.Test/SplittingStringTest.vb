a43318197ee54942780fdc558562a98538954e7b
kaneko <kaneko@daniel-soft.com>
2022-09-19 15:41:08 +0900

    引数がNothingだとエラーになるためエラー処理の追加

---
+++ ShoppingCart.Test/SplittingStringTest.vb
@@ -0,0 +1,10 @@
+﻿Imports NUnit.Framework
+Imports ShoppingCart
+Public Class SplittingStringTest
+    <Test>
+    Public Sub 引数に値が存在しなかった場合エラーと返す()
+        Dim inputContent As String = Nothing
+        Dim delimiter As String = " "
+        Assert.That(Sub() SplittingString.SplitIntoArrays(inputContent, delimiter), Throws.TypeOf(Of ArgumentException).And.Message.EqualTo("inputContentに値が存在しません"))
+    End Sub
+End Class
--- ShoppingCart/SplittingString.vb
+++ ShoppingCart/SplittingString.vb
@@ -8,7 +8,10 @@ Public Class StringExtraction
     ''' <param name="inputContent">分ける文字列</param>
     ''' <param name="delimiter">区切り文字</param>
     ''' <returns>配列に分けられた文字</returns>
-    Public Shared Function ExtractionString(inputContent As String, delimiter As String) As String()
+    Public Shared Function SplitIntoArrays(inputContent As String, delimiter As String) As String()
+        If IsNothing(inputContent) Then
+            Throw New ArgumentException("inputContentに値が存在しません")
+        End If
         Return inputContent.Split(CChar(delimiter))
     End Function
 End Class
